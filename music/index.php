f94159b958b55c4a8524120b391aa5034673a2e6
John Wiebe <johnjw59@gmail.com>
2015-06-14 20:26:47 -0700

    added navbar link and template index file

---
+++ music/index.php
@@ -0,0 +1,22 @@
+<?php 
+  require_once('../inc/init.inc');
+
+  $title = 'Album | Chapel';
+  $background_image = '/img/back_contact.jpg';
+  
+  require_once('../inc/header.inc');
+  require_once('../inc/nav.inc');
+?>
+  
+    <div id="about" class="content clearfix">
+      <div class="left">
+        <img class="title" src="/img/text-contact.png" alt="Chapel Album" />
+      </div>
+
+      <div class="right"> 
+
+      </div><!-- end of right -->
+    </div><!-- end of content -->
+
+    <?php require_once('../inc/social.inc'); ?>
+<?php require_once('../inc/footer.inc'); ?>
