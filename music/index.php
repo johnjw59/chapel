3a630e71b6d41af7558951f9b9c64d037a5d7acc
John Wiebe <john.wiebe@ayogo.com>
2015-06-18 20:00:17 -0700

    mostly finished the page. need to resize and optimize image links and get actual links

--- music/index.php
+++ music/index.php
@@ -2,7 +2,7 @@
   require_once('../inc/init.inc');
 
   $title = 'Album | Chapel';
-  $background_image = '/img/back_contact.jpg';
+  $background_image = '/img/back_album.jpg';
   
   require_once('../inc/header.inc');
   require_once('../inc/nav.inc');
@@ -10,11 +10,20 @@
   
     <div id="about" class="content clearfix">
       <div class="left">
-        <img class="title" src="/img/text-contact.png" alt="Chapel Album" />
+        <img class="title" src="/img/text-contact.png" alt="Chapel Album">
       </div>
 
-      <div class="right"> 
-
+      <div class="right center"> 
+        <h2>Chapel made an Album!</h2>
+        <p>You can download it from Bandcamp for <i>free</i>:</p>
+        <a href="http://bandcamp.com"><img src="https://s1.bcbits.com/img/buttons/bandcamp_130x27_white.png"></a>
+        <p>You can also support us by buying the album from iTunes:</p>
+        <a href=""><img src="/img/itunes.png" width="200px"></a>
+        <p>Or, if streaming is more your thing, you can check it out on these sites:</p>
+        <div class="img-wrapper clearfix">
+          <a href=""><img class="img-left" width="200px" src="/img/rdio.png"></a>
+          <a href=""><img class="img-right" width="200px" src="/img/spotify.jpg"></a>
+        </div>
       </div><!-- end of right -->
     </div><!-- end of content -->
 
