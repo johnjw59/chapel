4b2a0016384727eb5195f68a8d7a92d848bcf8b9
John Wiebe <johnjw59@gmail.com>
2015-06-23 20:49:49 -0700

    optimized images for web, created title image, added description to music page

--- music/index.php
+++ music/index.php
@@ -3,10 +3,10 @@
 
   $title = 'Music | Chapel';
   $background_image = '/img/back_music.jpg';
-
+  $description = "Download the Chapel Worship album from Bandcamp or iTunes or listen to it on Spotify or Rdio! Build Your Kingdom Here, One Thing Remains, All The Poor & Powerless, Great I Am, Anchor, In Tenderness, Came To My Rescue, Oceans, Amazing Grace, Set a Fire [Live]. Chapel's mission is to invite and unite a generation of young people to see God move in our city. Through passionate worship, inspiring and convicting messages, and a community of awesome people, we are taking a bold stand for Jesus as one voice."
   // Go straight to bandcamp until the album is up on itunes and friends.
   $scripts = array(
-    '<script type="text/javascript">window.location="https://chapelworship.bandcamp.com/";</script>',
+    '<script type="text/javascript">window.location="http://chapelworship.bandcamp.com/";</script>',
   );
   
   require_once('../inc/header.inc');
@@ -21,13 +21,13 @@
       <div class="pane-right center"> 
         <h2>Chapel made an Album!</h2>
         <p>You can download it from Bandcamp for <i>free</i>:</p>
-        <a href="http://bandcamp.com"><img src="https://s1.bcbits.com/img/buttons/bandcamp_130x27_white.png"></a>
+        <a href="http://chapelworship.bandcamp.com/"><img src="https://s1.bcbits.com/img/buttons/bandcamp_130x27_white.png" alt="Download the album on Bandcamp"></a>
         <p>You can also support us by buying the album from iTunes:</p>
-        <a href=""><img src="/img/itunes.png" width="170px"></a>
+        <a href=""><img src="/img/itunes.png" width="170px" alt="Buy the album on iTunes"></a>
         <p>Or, if streaming is more your thing, you can check it out on these sites:</p>
         <div class="img-wrapper clearfix">
-          <a href=""><img class="left" width="180px" src="/img/rdio.png"></a>
-          <a href=""><img class="right" width="180px" src="/img/spotify.jpg"></a>
+          <a href=""><img class="left" width="180px" src="/img/rdio.png" alt="Listen to the album on Spotify"></a>
+          <a href=""><img class="right" width="180px" src="/img/spotify.jpg" alt="Listen to the album on Rdio"></a>
         </div>
       </div><!-- end of right -->
     </div><!-- end of content -->
