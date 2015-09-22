5e01459da10a29d626c96dd19fd591e95aea0a86
John Wiebe <johnjw59@gmail.com>
2015-09-22 01:04:55 -0700

    moved the contact form from my portfolio over

--- contact/index.php
+++ contact/index.php
@@ -6,6 +6,8 @@
   
   require_once('../inc/header.inc');
   require_once('../inc/nav.inc');
+
+  require_once("../inc/mail_form.inc");
 ?>
   
     <div id="contact" class="content clearfix">
@@ -15,11 +17,32 @@
 
       <div class="pane-right">
         <p>
-          Under Contruction.
+          We'd love to hear from you! Please fill out the form below to send us a message.
         </p>
-        <p>
-          Please send all inquiries to <a href="mailto:chapelvancouver&#64;gmail&#46;com" target="_blank">chapelvancouver&#64;gmail&#46;com</a> in the meantime.
-        </p>
+        <?php
+          if (!empty($error_msg)) {
+            echo '<p class="error">ERROR: '. implode("<br />", $error_msg) . "</p>";
+          }
+          if ($result != NULL) {
+            echo '<p class="success">'. $result . "</p>";
+          }
+        ?>
+        <form action="<?php echo basename(__FILE__); ?>" method="post">
+          <noscript>
+            <input type="hidden" name="nojs" id="nojs" />
+          </noscript>
+
+          <label for="name">Name</label><br />
+          <input type="text" style="width:300px;" name="name" id="name" required><br />
+        
+          <label for="email">E-mail</label><br />
+          <input type="text" style="width:300px;" name="email" id="email" required><br />
+        
+          <label for="comments">Message</label><br />
+          <textarea name="comments" id="comments" rows="8" cols="50" style="width:530px" required></textarea><br />
+
+          <input type="submit" name="submit" id="submit" value="Send" <?php if (isset($disable) && $disable === true) echo ' disabled="disabled"'; ?> />
+        </form>
       </div><!-- end of right -->
     </div><!-- end of content -->
 
